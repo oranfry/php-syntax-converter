7a03ef0a47fb1337c5b9c61847e219c4cd8e999b
Oran Fry <oran.fry@propertysuite.co.nz>
2022-07-18 08:42:34 +1200

    Explain
    Simple script to show information about the tokens in a PHP script.

---
+++ explain.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+
+$code = stream_get_contents(STDIN);
+$tokens = PhpToken::tokenize($code);
+
+while ($token = array_shift($tokens)) {
+    echo "Line {$token->line}: {$token->getTokenName()} ('{$token->text}')", PHP_EOL;
+}
