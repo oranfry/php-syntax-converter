f05805f153bb0d6f5fbb0eae06038a727c3fa147
Oran Fry <oran.fry@propertysuite.co.nz>
2022-07-22 10:59:06 +1200

    Passthru
    The "null" implementation. Simply handles tokens by outputting their text. For a valid php file, the output will be equivalent to the input. Good starting point for a new converter.

---
+++ passthru.php
@@ -0,0 +1,49 @@
+#!/usr/bin/php
+<?php
+
+require __DIR__ . '/lib.php';
+
+function convert($code)
+{
+    $tokens = PhpToken::tokenize($code);
+    $handler = new to_alternative;
+    $signaller = (new signaller($handler, $tokens))->convert();
+}
+
+class to_alternative implements conversion_handler
+{
+    public function set_signaller(signaller $signaller): void
+    {
+    }
+
+    public function enter_context(array &$tokens, ?string $context_opener, ?array $context_closers)
+    {
+    }
+
+    public function enter_control(array &$tokens, string $control)
+    {
+    }
+
+    public function enter_control_body(array &$tokens, string $name)
+    {
+    }
+
+    public function handle_tokens(array &$tokens): void
+    {
+        echo array_shift($tokens)->text;
+    }
+
+    public function leave_control_body(array &$tokens, string $control, ?string $daisychain, $message): void
+    {
+    }
+
+    public function leave_control(array &$tokens, string $name, ?string $daisychain, $message): void
+    {
+    }
+
+    public function leave_context(array &$tokens, ?string $context_opener, ?string $context_closer, $message): void
+    {
+    }
+}
+
+convert(stream_get_contents(STDIN));
